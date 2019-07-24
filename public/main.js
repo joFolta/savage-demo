5e532f4c2b1d34edf26cf06fe0c8aaacf32d4b81
Johann Folta <johannfolta@gmail.com>
2019-07-23 21:05:01 -0400

    add clientside JS

--- public/main.js
+++ public/main.js
@@ -1,18 +1,30 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
+
+//THUMBS UP
+// Array.from(arrayName), new ES6 syntax
+// instead could use,
+// var thumbUp = document.querySelectorAll("fa-thumbs-up");
+// thumbUp.forEach(function(element) {
+
 
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
+        console.log("up")
+        //"this" is clickedOn thumb up <i>icon</i>
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
+        //parseFloat to change to StringValue from DOM to NumberValue
+        const countUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('thumbUp', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
+          //changing to JSON format to send from client to server
           body: JSON.stringify({
             'name': name,
             'msg': msg,
-            'thumbUp':thumbUp
+            'thumbUp':countUp
           })
         })
         .then(response => {
@@ -25,10 +37,45 @@ Array.from(thumbUp).forEach(function(element) {
       });
 });
 
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    //"this" is clickedOn thumb up <i>icon</i>
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    //parseFloat to change to StringValue from DOM to NumberValue
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    console.log(`thumbUp ${thumbUp}`)
+    fetch('messagesDown', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      //changing to JSON format to send from client to server
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbUp':thumbUp
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
+//TRASH CAN
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
+        //name and message targed
+            //this = what you clicked on
+            //parentNode = go up a level
+            //parentNode = go up a level
+            //childNode = go INTO the level (BY INDEX)
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
+        // messages end point
         fetch('messages', {
           method: 'delete',
           headers: {
